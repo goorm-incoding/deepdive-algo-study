c64608c1410912af7d09ecd0b7f2221e9ffe1a04
Lee Seungwook <leesw_0924@icloud.com>
2025-05-08 13:07:47 +0900

    solve: 자료 구조 - 아이템 교환

---
+++ 9.data_structure/이승욱/Problem3.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Problem3
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> goorm = new HashSet<>();
+        Set<String> friend = new HashSet<>();
+
+        // 구름이가 갖고 있는 아이템을 set에 담음
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) goorm.add(st.nextToken());
+
+        // 친구가 갖고 있는 아이템을 set에 담음
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) friend.add(st.nextToken());
+
+        // m번의 교환 요청 처리
+        for (int i = 0; i < m; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            String itemA = st.nextToken();
+            String itemB = st.nextToken();
+
+            // a를 구름이가 갖고 있고, b를 친구가 갖고 있으면 서로 교환
+            if (goorm.contains(itemA) && friend.contains(itemB))
+            {
+                goorm.remove(itemA);
+                goorm.add(itemB);
+                friend.remove(itemB);
+                friend.add(itemA);
+            }
+        }
+
+        // 아이템의 이름을 사전순으로 출력하기 위해 리스트에 담고 정렬
+        List<String> answer = new ArrayList<>(goorm);
+        Collections.sort(answer);
+
+        for (String item : answer) System.out.print(item + " ");
+    }
+}
